571410cb3dc7c0985f13eebe65b88b8c2f05b133
luizsdev <luizsdev@gmail.com>
2023-03-23 03:13:20 -0300

    chore: new libs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = module.exports = {
+	reactStrictMode: true,
+	images: {
+		domains: ['rickandmortyapi.com'],
+	},
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx}',
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+
+		// Or if using `src` directory:
+		'./src/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			animation: {
+				'fade-in-left': 'fadeInLeft 0.5s ease-out',
+				'fade-in-left-slow': 'fadeInLeft 1s ease-out',
+			},
+			keyframes: {
+				fadeInLeft: {
+					'0%': {
+						transform: 'translateX(-5%)',
+						opacity: '0',
+					},
+					'100%': {
+						transform: 'translateX(0)',
+						opacity: '1',
+					},
+				},
+			},
+		},
+	},
+	plugins: [],
+};
