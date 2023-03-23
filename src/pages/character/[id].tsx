1db0e4e370dd0538abd918369611accb7b22cc13
luiz <luizsdev@gmail.com>
2023-03-23 14:13:17 -0300

    fix: fix production

--- src/pages/character/[id].tsx
+++ src/pages/character/[id].tsx
@@ -1,7 +1,8 @@
 import { Character } from "@/types/Character";
 import { Episode } from "@/types/Episode";
-import { Clock } from "@phosphor-icons/react";
-
+import { AiOutlineClockCircle, AiOutlineArrowLeft } from "react-icons/ai";
+import { GoLocation } from "react-icons/go";
+import { BiPlanet } from "react-icons/bi";
 import { GetServerSideProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
@@ -24,21 +25,21 @@ export default function Page({ character, episode }: PageProps) {
   };
   return (
     <>
-      <main className="h-screen w-screen bg-slate-800 flex items-center justify-center  ">
-        <div className="2xl:h-5/6 xl:h-4/6 lg:h-3/6  w-3/6 sm:w-2/6 bg-slate-600  rounded-3xl flex item-start justify-start flex-col gap-0  xl:gap-10 ">
-          <div className=" flex flex-col sm:flex-row items-center justify-start ">
+      <main className="h-screen w-screen bg-slate-800 flex items-center justify-center gap-10 ">
+        <div className="h-5/6 w-2/6 bg-slate-600  rounded-3xl flex item-start justify-start flex-col gap-5 ">
+          <div className=" flex items-center justify-start ">
             <Image
               width={150}
-              height={150}
-              className=" rounded-2xl sm:rounded-t-0 lg:w-40 "
+              height={500}
+              className="rounded-tl-3xl rounded-br-3xl"
               alt="character"
               src={character?.image}
             />
-            <div className="w-full flex flex-col items-center justify-center sm:gap-1">
-              <h1 className="xl:text-3xl lg:text-xl md:text-md text-sm 2xl:text-4xl  text-white flex items-center sm:gap-3">
+            <div className="w-full flex flex-col items-center justify-center gap-1">
+              <h1 className="xl:text-3xl lg:text-xl md:text-md text-sm  text-white flex items-center gap-2">
                 {character?.name}
               </h1>
-              <div className="xl:text-3xl lg:text-xl 2xl:text-4xl  md:text-md text-sm flex flex-row items-center justify-center gap-2 text-slate-400">
+              <div className="xl:text-3xl lg:text-xl md:text-md text-sm flex flex-row items-center justify-center gap-2 text-slate-400">
                 {checkAlive()}
                 {character?.status}
                 {" - "}
@@ -48,26 +49,29 @@ export default function Page({ character, episode }: PageProps) {
           </div>
           <div className="flex flex-col items-center justify-center  mt-2">
             <div className="flex flex-col gap-12">
-              <div className="flex flex-col items-center justify-center  sm:gap-3 ">
-                <h1 className="text-white xl:text-xl 2xl:text-4xl  lg:text-xl md:text-md text-sm font-bold flex flex-row gap-2"></h1>
-                <p className=" xl:text-xl lg:text-xl 2xl:text-3xl md:text-md text-sm text-slate-400 font-normal">
+              <div className="flex flex-col items-center justify-center gap-3">
+                <h1 className="text-white xl:text-3xl lg:text-xl md:text-md text-sm font-bold flex flex-row gap-2">
+                  <GoLocation size={30} /> Last Known Location:
+                </h1>
+                <p className=" xl:text-3xl lg:text-xl md:text-md text-sm text-slate-400 font-normal">
                   {character.location.name}
                 </p>
               </div>
-              <div className="flex flex-col items-center justify-center sm:gap-3">
-                <h1 className="text-white xl:text-xl 2xl:text-4xl lg:text-xl md:text-md text-sm font-bold flex flex-row gap-2">
+              <div className="flex flex-col items-center justify-center gap-3">
+                <h1 className="text-white xl:text-3xl lg:text-xl md:text-md text-sm font-bold flex flex-row gap-2">
                   {" "}
+                  <BiPlanet size={30} /> Origin:
                 </h1>
-                <p className=" xl:text-xl lg:text-xl 2xl:text-3xl md:text-md text-sm text-slate-400 font-normal">
+                <p className=" xl:text-3xl lg:text-xl md:text-md text-sm text-slate-400 font-normal">
                   {character.origin.name}
                 </p>
               </div>
-              <div className="flex flex-col items-center justify-center sm:gap-3">
-                <h1 className="text-white xl:text-xl 2xl:text-4xl lg:text-xl md:text-md text-sm font-bold flex flex-row gap-2">
+              <div className="flex flex-col items-center justify-center gap-3">
+                <h1 className="text-white xl:text-3xl lg:text-xl md:text-md text-sm font-bold flex flex-row gap-2">
                   {" "}
+                  <AiOutlineClockCircle size={30} /> First Seen in:
                 </h1>
-                <p className=" xl:text-xl lg:text-xl 2xl:text-3xl md:text-md text-sm text-slate-400 font-normal">
-                  <Clock size={30} />
+                <p className=" xl:text-3xl lg:text-xl md:text-md text-sm text-slate-400 font-normal">
                   {episode.name}
                   {" - "}
                   {episode.episode}
@@ -75,13 +79,14 @@ export default function Page({ character, episode }: PageProps) {
               </div>
             </div>
           </div>
-          <div className="flex flex-row items-start justify-center max-h-10 mt-2 sm:mt-0">
+          <div className="flex flex-row items-start justify-center max-h-10 ">
             <Link
-              href={"/#title"}
+              href={"/aaa"}
               type="button"
-              className="flex  justify-center h-10 items-center sm:gap-2  w-1/2 2xl:w-2/3 2xl:mt-10 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm  text-center mr-2  mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="flex  justify-center h-10 items-center gap-2    w-1/2 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm  text-center mr-2  mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              <p className="xl:text-xl 2xl:text-3xl lg:text-xl md:text-md text-xs  font-bold">
+              <AiOutlineArrowLeft size={30} />{" "}
+              <p className="xl:text-3xl lg:text-xl md:text-md text-sm font-bold">
                 Go Back
               </p>
             </Link>
