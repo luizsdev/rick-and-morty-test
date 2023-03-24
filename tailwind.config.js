89d264c8939b534a8187065f680374187b77b867
luiz <luizsdev@gmail.com>
2023-03-24 08:21:48 -0300

    feat: error page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./app/**/*.{js,ts,jsx,tsx}',
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}',
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
 
-		// Or if using `src` directory:
-		'./src/**/*.{js,ts,jsx,tsx}',
-	],
-	theme: {
-		extend: {
-			animation: {
-				'fade-in-left': 'fadeInLeft 0.5s ease-out',
-				'fade-in-left-slow': 'fadeInLeft 1s ease-out',
-				'fade-in-left-fast': 'fadeInLeft 0.2s ease-out',
-			},
-			keyframes: {
-				fadeInLeft: {
-					'0%': {
-						transform: 'translateX(-5%)',
-						opacity: '0',
-					},
-					'100%': {
-						transform: 'translateX(0)',
-						opacity: '1',
-					},
-				},
-			},
-		},
-	},
-	plugins: [],
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      dropShadow: {
+        background: "    --tw-drop-shadow: drop-shadow(4px 4px 0px #08bae3);",
+      },
+      animation: {
+        "fade-in-left": "fadeInLeft 0.5s ease-out",
+        "fade-in-left-slow": "fadeInLeft 1s ease-out",
+        "fade-in-left-fast": "fadeInLeft 0.2s ease-out",
+      },
+      keyframes: {
+        fadeInLeft: {
+          "0%": {
+            transform: "translateX(-5%)",
+            opacity: "0",
+          },
+          "100%": {
+            transform: "translateX(0)",
+            opacity: "1",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
 };
