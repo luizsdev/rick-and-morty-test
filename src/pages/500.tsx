0e093df2f307be16b393752991a1cd63f72ef5fe
luiz <luizsdev@gmail.com>
2023-03-24 09:44:26 -0300

    chore: change error page button font size

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -27,7 +27,7 @@ export default function Error() {
             <Link
               href={"/"}
               type="button"
-              className="text-gray-900 mt-5 flex items-center justify-center flex-row gap-2 w-32 h-12 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+              className="text-gray-800  mt-5 flex items-center justify-center flex-row gap-2 w-32 h-12 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-teal-700 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2"
             >
               <BiArrowBack /> Home
             </Link>
